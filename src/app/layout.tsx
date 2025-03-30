5a9554eef8f3db0710d57da29a34babd92a8001e
Ani Pottammal <ap7564@nyu.edu>
2025-03-30 14:14:43 -0400

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "NYU/NYC Purity Test",
-  description: "Find out how pure you are with the NYU/NYC Purity Test!",
+  title: "NYU Purity Test",
+  description: "NYU's Purity Test",
+  icons: {
+    icon: "/images/favicon.ico",
+    shortcut: "/images/favicon.ico",
+    apple: "/images/favicon.ico",
+    other: {
+      rel: "icon",
+      url: "/images/favicon.ico",
+    },
+  },
 };
 
 export default function RootLayout({
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/images/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
